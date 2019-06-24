85a72ad9c648deb8147853dbda38bf05483dc73a
zhanglei236 <zhanglei51@jd.com>
2019-06-24 21:38:23 +0800

    init

---
+++ src/main/java/cn/smile/spring/container/bean/factoryBean/Car.java
@@ -0,0 +1,15 @@
+package cn.smile.spring.container.bean.factoryBean;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Car {
+    private int maxSpeed;
+    private String brand;
+    private double price;
+}
---
+++ src/test/java/cn/smile/spring/container/bean/VectorTest.java
@@ -0,0 +1,73 @@
+package cn.smile.spring.container.bean;
+
+import org.junit.Test;
+
+import java.util.Vector;
+
+public class VectorTest {
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+    @Test
+    public void normalVectorTest(){
+        while(true){
+            for(int i = 0;i < 10 ;i++){
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                public void run() {
+                    for(int i = 0;i < vector.size() ;i++){
+                        vector.remove(i);
+                    }
+                }
+            });
+
+            Thread getThread = new Thread(new Runnable() {
+                public void run() {
+                    for(int i = 0;i < vector.size() ;i++){
+                        System.out.println(vector.get(i));
+                    }
+                }
+            });
+
+            removeThread.start();
+            getThread.start();
+
+            while(Thread.activeCount() > 20);
+        }
+    }
+
+    @Test
+    public void concorrentVectorTest(){
+        while(true){
+            for(int i = 0;i < 10 ;i++){
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                public void run() {
+                    synchronized (vector){
+                        for(int i = 0;i < vector.size() ;i++){
+                            vector.remove(i);
+                        }
+                    }
+                }
+            });
+
+            Thread getThread = new Thread(new Runnable() {
+                public void run() {
+                    synchronized (vector) {
+                        for (int i = 0; i < vector.size(); i++) {
+                            System.out.println(vector.get(i));
+                        }
+                    }
+                }
+            });
+
+            removeThread.start();
+            getThread.start();
+
+           while(Thread.activeCount() > 20);
+        }
+    }
+}
